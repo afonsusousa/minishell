71130023346156bcf76d20f085bd5b89fa05dca8
afonsusousa <afonso.rocha@outlook.com>
2025-10-29 02:25:24 +0000

    quote machine tuning

--- src/subst_machine.c
+++ src/subst_machine.c
@@ -19,12 +19,12 @@ typedef enum s_state
 
 typedef struct s_quote_machine
 {
-    const char  *str;
-    char         ch;
-    size_t      str_pos;
-    size_t      str_len;
     t_state     curr;
     t_state     prev;
+    const char  *str;
+    size_t      str_pos;
+    size_t      str_len;
+    char         ch;
     char        buffer[ARG_MAX];
     size_t      buff_pos;
 } t_quote_machine;
@@ -37,18 +37,11 @@ void sm_advance(t_quote_machine *sm)
         sm->ch = '\0';
 }
 
-void sm_retrocede(t_quote_machine *sm)
-{
-    if (sm->str_pos)
-        sm->ch = sm->str[--sm->str_pos];
-    else
-        sm->ch = sm->str[0];
-}
-
 void sm_trasition(t_quote_machine *sm, t_state new)
 {
     sm->prev = sm->curr;
     sm->curr = new;
+    sm_advance(sm);
 }
 
 void sm_laststate(t_quote_machine *sm)
@@ -61,6 +54,12 @@ void sm_laststate(t_quote_machine *sm)
 const char *sm_getstr(t_quote_machine *sm)
 {
     return (&(sm->str[sm->str_pos]));
+}
+
+void sm_consume(t_quote_machine *sm)
+{
+    sm->buffer[sm->buff_pos++] = sm->ch;
+    sm_advance(sm);
 }
 
 void    sm_init(t_quote_machine *sm, const char *str)
@@ -81,6 +80,7 @@ char *expanded(const t_envp *env, const char *str)
     t_quote_machine sm;
     const char *var;
 
+    sm_init(&sm, str);
     while (sm.ch)
     {
         if (sm.curr == DEFAULT)
@@ -92,7 +92,7 @@ char *expanded(const t_envp *env, const char *str)
             else if (sm.ch == '$')
                 sm_trasition(&sm, IN_VAR);
             else
-                sm.buffer[sm.buff_pos++] = sm.ch;
+                sm_consume(&sm);
         }
         else if (sm.curr == IN_DQ)
         {
@@ -101,35 +101,29 @@ char *expanded(const t_envp *env, const char *str)
             else if (sm.ch == '"')
                 sm_trasition(&sm, DEFAULT);
             else
-                sm.buffer[sm.buff_pos++] = sm.ch;
+                sm_consume(&sm);
         }
         else if (sm.curr == IN_SQ)
         {
             if (sm.ch == '\'')
                 sm_trasition(&sm, DEFAULT);
             else
-                sm.buffer[sm.buff_pos++] = sm.ch;
+                sm_consume(&sm);
         }
         else if (sm.curr == IN_VAR)
         {
             if (!is_valid(sm.ch))
             {
                 while (!isspace(sm.ch) && sm.ch)
-                {
-                    sm.buffer[sm.buff_pos++] = sm.ch;
-                    sm_advance(&sm);
-                }
+                    sm_consume(&sm);
                 sm_laststate(&sm);
-                continue ;
             }
             var = envp_getvar_value(env, sm_getstr(&sm));
             ft_strlcat(sm.buffer, var, ft_strlen(sm.buffer) + ft_strlen(var) + 1);
             while (is_valid(sm.ch))
                 sm_advance(&sm);
             sm_laststate(&sm);
-            continue ;
         }
-        sm_advance(&sm);
     }
     return (ft_strdup(sm.buffer));
 }
