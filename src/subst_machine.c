78cc142f1164286d91de7e0a2e83d0ea27c38418
afonsusousa <afonso.rocha@outlook.com>
2025-10-29 02:03:04 +0000

    fixed the quote machine

--- src/subst_machine.c
+++ src/subst_machine.c
@@ -25,6 +25,8 @@ typedef struct s_quote_machine
     size_t      str_len;
     t_state     curr;
     t_state     prev;
+    char        buffer[ARG_MAX];
+    size_t      buff_pos;
 } t_quote_machine;
 
 void sm_advance(t_quote_machine *sm)
@@ -35,12 +37,6 @@ void sm_advance(t_quote_machine *sm)
         sm->ch = '\0';
 }
 
-void sm_trasition(t_quote_machine *sm, t_state new)
-{
-    sm->prev = sm->curr;
-    sm->curr = new;
-}
-
 void sm_retrocede(t_quote_machine *sm)
 {
     if (sm->str_pos)
@@ -48,6 +44,13 @@ void sm_retrocede(t_quote_machine *sm)
     else
         sm->ch = sm->str[0];
 }
+
+void sm_trasition(t_quote_machine *sm, t_state new)
+{
+    sm->prev = sm->curr;
+    sm->curr = new;
+}
+
 void sm_laststate(t_quote_machine *sm)
 {
     sm->curr = sm->curr ^ sm->prev;
@@ -60,21 +63,24 @@ const char *sm_getstr(t_quote_machine *sm)
     return (&(sm->str[sm->str_pos]));
 }
 
+void    sm_init(t_quote_machine *sm, const char *str)
+{
+    sm->str = str;
+    sm->ch = *str;
+    sm->str_pos = 0;
+    sm->buff_pos = 0;
+    sm->curr = DEFAULT;
+    sm->prev = DEFAULT;
+    sm->str_len = ft_strlen(str);
+    ft_bzero(sm->buffer, ARG_MAX);
+}
+
+//TODO: is_valid needs fixing (allows much more characters!!)
 char *expanded(const t_envp *env, const char *str)
 {
     t_quote_machine sm;
-    size_t  i;
-    char buffer[ARG_MAX];
     const char *var;
 
-    i = 0;
-    sm.str = str;
-    sm.ch = *str;
-    sm.str_pos = 0;
-    sm.curr = DEFAULT;
-    sm.prev = DEFAULT;
-    sm.str_len = ft_strlen(str);
-    ft_bzero(buffer, ARG_MAX);
     while (sm.ch)
     {
         if (sm.curr == DEFAULT)
@@ -86,7 +92,7 @@ char *expanded(const t_envp *env, const char *str)
             else if (sm.ch == '$')
                 sm_trasition(&sm, IN_VAR);
             else
-                buffer[i++] = sm.ch;
+                sm.buffer[sm.buff_pos++] = sm.ch;
         }
         else if (sm.curr == IN_DQ)
         {
@@ -95,14 +101,14 @@ char *expanded(const t_envp *env, const char *str)
             else if (sm.ch == '"')
                 sm_trasition(&sm, DEFAULT);
             else
-                buffer[i++] = sm.ch;
+                sm.buffer[sm.buff_pos++] = sm.ch;
         }
         else if (sm.curr == IN_SQ)
         {
             if (sm.ch == '\'')
                 sm_trasition(&sm, DEFAULT);
             else
-                buffer[i++] = sm.ch;
+                sm.buffer[sm.buff_pos++] = sm.ch;
         }
         else if (sm.curr == IN_VAR)
         {
@@ -110,19 +116,20 @@ char *expanded(const t_envp *env, const char *str)
             {
                 while (!isspace(sm.ch) && sm.ch)
                 {
-                    buffer[i++] = sm.ch;
+                    sm.buffer[sm.buff_pos++] = sm.ch;
                     sm_advance(&sm);
                 }
                 sm_laststate(&sm);
                 continue ;
             }
             var = envp_getvar_value(env, sm_getstr(&sm));
-            ft_strlcat(buffer, var, ft_strlen(buffer) + ft_strlen(var) + 1);
-            while (is_valid(sm.ch) && sm.ch != '$')
+            ft_strlcat(sm.buffer, var, ft_strlen(sm.buffer) + ft_strlen(var) + 1);
+            while (is_valid(sm.ch))
                 sm_advance(&sm);
             sm_laststate(&sm);
+            continue ;
         }
         sm_advance(&sm);
     }
-    return (ft_strdup(buffer));
+    return (ft_strdup(sm.buffer));
 }
