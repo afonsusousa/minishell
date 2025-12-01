0f9323b124077fa0033c6876402b76ef362e01e3
afonsusousa <afonso.rocha@outlook.com>
2025-12-01 19:27:48 +0000

    better execve error log

--- src/executor/cores/command/command.c
+++ src/executor/cores/command/command.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <errno.h>
 
 #include "../../../../includes/minishell.h"
 #include "../../../../includes/executor.h"
@@ -26,10 +27,30 @@ static int exec_assignments(t_minishell* sh, const char **a, bool context)
     return (0);
 }
 
+static void execve_error(const char *cmd)
+{
+    struct stat path_stat;
+
+    write(2, "minishell: ", 11);
+    if (stat(cmd, &path_stat) == 0 && S_ISDIR(path_stat.st_mode))
+    {
+        write(2, cmd, ft_strlen(cmd));
+        write(2, ": Is a directory\n", 17);
+        exit(126);
+    }
+    if (errno == ENOENT)
+    {
+        write(2, cmd, ft_strlen(cmd));
+        write(2, ": command not found\n", 20);
+        exit(127);
+    }
+    perror(cmd);
+    exit(126);
+}
+
 int execve_wrapper(t_minishell* sh, char** argv, int argc)
 {
     char** env_arr;
-    struct stat path_stat;
 
     if (!argv || !argv[0])
         return (0);
@@ -40,16 +61,8 @@ int execve_wrapper(t_minishell* sh, char** argv, int argc)
     argv[0] = find_path(sh, argv[0]);
     minishell_free(sh);
     execve(argv[0], argv, env_arr);
-    write(2, "minishell: ", 11);
-    write(2, argv[0], ft_strlen(argv[0]));
-    if (stat(argv[0], &path_stat) == 0 && S_ISDIR(path_stat.st_mode))
-    {
-        write(2, ": Is a directory\n", 17);
-        exit(126);
-    }
-    write(2, ": ", 2);
-    perror(NULL);
-    exit(127);
+    execve_error(argv[0]);
+    return (0);
 }
 
 int exec_command(t_minishell* sh, const t_ast* core)
